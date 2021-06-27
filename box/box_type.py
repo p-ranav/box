a73e999e09585470735978307d646ea9fc16cbca
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-06-27 12:14:15 -0500

    Deleted old source files

--- box/box_type.py
+++
@@ -1,7 +0,0 @@
-
-BOX_TYPE_FUNCTION = "function"
-BOX_TYPE_FUNCTION_INPUTS = "function_inputs"
-BOX_TYPE_FUNCTION_OUTPUTS = "function_outputs"
-BOX_TYPE_COMMENT = "comment"
-BOX_TYPE_NUMERIC = "numeric"
-BOX_TYPE_STRING_LITERAL = "string_literal"
--- box/nodes/constant_node.py
+++
@@ -1,9 +0,0 @@
-class ConstantNode:
-    def __init__(self, box_ref, name, value):
-        self.box_ref = box_ref
-        self.name = name
-        self.value = value
-        self.value_type = ""
-
-    def to_python(self):
-        return self.name + ' = ' + str(self.value)
