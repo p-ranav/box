9d2ee837507d8e5ea4006ff5117c4def85b5df12
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-07-01 10:54:11 -0500

    Added way to save generated python code

--- src/box/main.py
+++ src/box/main.py
@@ -1,7 +1,6 @@
 import argparse
 from box.parser import Parser
 from box.generator import Generator
-from box.importer import Importer
 import os
 
 
@@ -10,17 +9,17 @@ def main():
     parser = argparse.ArgumentParser(description="Box interpreter")
     parser.add_argument("path", help="Path to box file")
     parser.add_argument("-v", action="store_true", help="Toggle verbosity")
+    parser.add_argument("-o", help="Path where to store the generated Python code")    
     parser.add_argument("-e", nargs="*", help="Arguments to pass to box function")
-    parser.add_argument("-I", help="Directory containing function graphs to import")    
+
+    # TODO: Figure out the design for imports (or even to support it)
+    # parser.add_argument("-I", help="Directory containing function graphs to import")
+    
     args = parser.parse_args()
 
     filename = args.path
     eval_args = args.e
     verbosity = args.v
-    import_path = args.I
-
-    importer = Importer(import_path)
-
     path = os.path.join(os.getcwd(), filename)
     parser = Parser(path)
     generator = Generator(parser)
@@ -30,8 +29,11 @@ def main():
     if args.v:
         print(code)
 
-    exec(code)
+    if args.o:
+        with open(os.path.abspath(args.o), 'w') as output:
+            output.write(code)
 
+    exec(code)
 
 if __name__ == "__main__":
     main()
